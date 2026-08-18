2aa4945411de18fff7097d910631b57c50ad3260
Carlos Torres Paredes <carltpr@gmail.com>
2026-08-18 11:47:31 +0200

    FormConcluso

--- app.js
+++ app.js
@@ -253,26 +253,46 @@ function onCambiaStato(id, nuovoStato) {
   return true;
 }
 
+function addId() {
+  let id = Date.now() + Math.floor(Math.random() * 100);
+  console.log(id);
+  return id;
+}
+
 function addVuln() {
   const name_el = document.getElementById("vul_name");
-  let nome = name_el.value;
-  if (!nome) {
+  let nameVal = name_el.value;
+  if (!nameVal) {
     alert("Inserisci un nome valido");
     return false;
   }
 
-  console.log(nome);
+  console.log(nameVal);
+  return nameVal;
 }
 
 function addVulSev() {
-  const severita_el = document.querySelector('input[name="severita"]:checked');
-  if (!severita_el) {
+  const sev_el = document.querySelector('input[name="severita"]:checked');
+  if (!sev_el) {
     alert("Seleziona una severità");
     return false;
   }
- let severitaVal = severita_el.value;
 
-  console.log(severitaVal);
+  let sevVal = sev_el.value;
+  console.log(sevVal);
+  return sevVal;
+}
+
+function addSys() {
+  const sys_el = document.getElementById("vul_sys");
+  let sysVal = sys_el.value;
+  if (!sysVal) {
+    alert("Inserisci il sistema");
+    return false;
+  }
+
+  console.log(sysVal);
+  return sysVal;
 }
 
 function addVulst() {
@@ -281,15 +301,49 @@ function addVulst() {
     alert("Seleziona uno stato");
     return false;
   }
-  let statoVal = state_el.value;
 
-  console.log(statoVal);
+  let stateVal = state_el.value;
+  console.log(stateVal);
+  return stateVal;
 }
 
 function addVuld() {
   const desc_el = document.getElementById("vul_desc");
   let descVal = desc_el.value;
   console.log(descVal);
+  return descVal;
+}
+
+function addDate(){
+  const date_el = document.getElementById("vul_date").value;
+  if (!date_el) {
+    alert("Inserisci una data valida");
+    return false;
+  }
+
+  let dateVal = date_el;
+  console.log(dateVal);
+  return dateVal;
+}
+
+function pushVul() {
+  let vul = {
+    id: addId(),
+    nome: addVuln(),
+    severita: addVulSev(),
+    sistema: addSys(),
+    stato: addVulst(),
+    descrizione: addVuld(),
+    data: addDate()
+  };
+
+const valido = Object.values(vul).every(valore => Boolean(valore));
+if(!valido) return false;
+
+vulnerabilita.push(vul);
+salvaVulnerabilita(vulnerabilita);
+renderTable(vulnerabilita);
+document.getElementById("formVulnerabilita").reset();
 }
 
 // --- avvio ---
